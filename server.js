bd1ab4ad65571b9ee2e49e4fd8d1255dafc02106
TopSGW <web.smartdev22@gmail.com>
2022-12-07 11:11:16 -0800

    third

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
+const hostname = '34.238.251.170'
 const port = process.env.PORT || 3000
 
 const app = next({ dev, hostname, port })
